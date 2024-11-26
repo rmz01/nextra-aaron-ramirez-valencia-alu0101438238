f4401ddc3c55d173275ded2aafbeeac5a2fd16a3
Aarón <72323217+rmz01@users.noreply.github.com>
2024-11-26 22:18:19 +0000

    catapi y githubapi

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 [![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=16739194)
 
-
-Repositorio de la practica con el despliegue: [(https://github.com/rmz01/nextra-aaron-ramirez-valencia-alu0101438238)]
+![Repositorio de la practica con el despliegue](https://github.com/rmz01/nextra-aaron-ramirez-valencia-alu0101438238)
--- pages/api/data.js
+++ pages/api/data.js
@@ -1,3 +1,21 @@
+/*
 export default function handler(req, res) {
-    res.status(200).json({ message: 'Hello, World!', date: new Date().toISOString() })
-  }
+   res.status(200).json({ message: 'Hello, World!', date: new Date().toISOString() })
+ }
+*/
+
+/*
+export default async (req, res) => {
+  const response = await fetch('https://api.thecatapi.com/v1/images/search');
+  const data = await response.json();
+ 
+ 
+  res.status(200).json(data);
+ }
+*/
+
+export default async (req, res) => {
+  const response = await fetch('https://api.github.com/users/rmz01/repos')
+  const data = await response.json()
+  res.status(200).json(data)
+}
