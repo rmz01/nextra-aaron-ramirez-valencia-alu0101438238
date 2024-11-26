284a69fbe5600a7df5411b6ab702c9b674c7c79d
Aarón <72323217+rmz01@users.noreply.github.com>
2024-11-26 22:23:32 +0000

    terminado

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,17 +1,15 @@
 /*
 export default function handler(req, res) {
-   res.status(200).json({ message: 'Hello, World!', date: new Date().toISOString() })
- }
+  res.status(200).json({ message: 'Hello, World!', date: new Date().toISOString() })
+}
 */
 
 /*
 export default async (req, res) => {
   const response = await fetch('https://api.thecatapi.com/v1/images/search');
   const data = await response.json();
- 
- 
   res.status(200).json(data);
- }
+}
 */
 
 export default async (req, res) => {
