5f13d02198ed6d4fe93fa7f591fda69735e988ad
Aarón <72323217+rmz01@users.noreply.github.com>
2024-11-27 10:40:44 +0000

    no subido

--- pages/api/data.js
+++ pages/api/data.js
@@ -12,8 +12,34 @@ export default async (req, res) => {
 }
 */
 
+/*
 export default async (req, res) => {
   const response = await fetch('https://api.github.com/users/rmz01/repos')
   const data = await response.json()
   res.status(200).json(data)
+}
+*/
+
+export default async (req, res) => {
+  const { username } = req.query;
+  const token = 'GITHUB_TOKEN'; 
+
+  if (!username) {
+    return res.status(400).json({ error: 'Username is required' });
+  }
+
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}/repos`, {
+      headers: {
+        'Authorization': `token ${token}`
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
