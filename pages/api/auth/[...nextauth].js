d0db149828c562e94ea57e932a9c2aa65de3c00d
Aarón <72323217+rmz01@users.noreply.github.com>
2024-12-04 11:45:28 +0000

    modifciado el nextauth.js para cambiar el uso de next a nextra

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,14 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
  
-export default NextAuth({
+export const authOptions = {
   providers: [
-    GithubProvider({
+    GithubProvider.default({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-})
+  // add more options here
+}
+
+export default NextAuth.default(authOptions)
