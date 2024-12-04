b95e27d7c60f2053dc5d933126a7a5da091ccf9f
Aarón <72323217+rmz01@users.noreply.github.com>
2024-12-04 11:01:38 +0000

    comenzando nextauth

--- middleware.js
+++ middleware.js
@@ -1,7 +1,9 @@
-// FILE: pages/_middleware.js
 import { NextResponse } from 'next/server'
+export { default } from "next-auth/middleware"
 
 export function middleware(req) {
   console.log(`Request made to: ${req.nextUrl.pathname}`)
   return NextResponse.next()
 }
+
+export const config = { matcher: ["/clases", "/labs"] }
