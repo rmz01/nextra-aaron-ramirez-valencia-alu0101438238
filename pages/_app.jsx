908160b3d44f38a545fd24d4d68108124dd9cedf
Aarón <72323217+rmz01@users.noreply.github.com>
2024-12-04 12:09:36 +0000

    anadida la componente login button y cambiado el app.js

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,4 @@
-// pages/_app.js
+/*
 import '../styles/bleed.css'
 
 function MyApp({ Component, pageProps }) {
@@ -7,3 +7,20 @@ function MyApp({ Component, pageProps }) {
 
 export default MyApp
 
+ Old code
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+*/
+import { SessionProvider } from "next-auth/react"
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
